f9d9d8f96df521dfed459989cb470cca0f3e61d7
Noor Ali <nooralib180@gmail.com>
2022-06-11 13:35:47 +0500

    class component & fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super(); // /calling parent constructor.
+
+    // / state property is present in Component class there for we call super(), constructor of parent class to get access to state property. 
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => this.setState({monsters: data}));
+  }
+
+  render(){
+    return(
+      <div className = 'main'>
+        {
+          this.state.monsters.map((monster) => <h1 key = {monster.id}>{monster.name}</h1>)
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
