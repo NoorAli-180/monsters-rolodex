aad1ca59a86423fc84e9fcd64642719dc02c14ee
Noor Ali <nooralib180@gmail.com>
2022-06-11 17:38:49 +0500

    card component rendered

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
+import './App.css';
+import { CardList } from './components/card-list/CardList.jsx';
+
 class App extends React.Component{
   constructor(){
-    super(); // /calling parent constructor.
+    super();
+    // /calling parent constructor.
 
-    // / state property is present in Component class there for we call super(), constructor of parent class to get access to state property. 
-
+    // / state property is present in Component class there for we call super(), constructor of parent class to get access to state property.
     this.state = {
       monsters: [],
-    };
+    }
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(data => this.setState({monsters: data}));
+    .then(users => {this.setState({monsters: users})});
   }
 
   render(){
     return(
-      <div className = 'main'>
-        {
-          this.state.monsters.map((monster) => <h1 key = {monster.id}>{monster.name}</h1>)
-        }
+      <div className="App">
+        <CardList monsters = {this.state.monsters} />
       </div>
     )
   }
